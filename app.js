68edf53f8e5ac861a75740c0b57bb36985100023
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-10 09:09:45 +0500

    loop practice with switch

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ for (var i = 1; i <= 10; i++){
         console.log(i);
     }
 }
+
+for (var i = 1; i <= 10; i++){
+    switch(i){
+        case 1:
+        console.log("Gold Medal");
+        break;
+        case 2:
+        console.log("Silver Medal");
+        break;
+        case 3:
+        console.log("Bronze Medal");
+        break;
+        default:
+        console.log(i);
+    }
+}
