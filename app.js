4bd9baeafba0c99d619481af4188dcea16cafdab
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-10 15:48:16 +0500

    array with if else and loop

--- app.js
+++ app.js
@@ -33,10 +33,22 @@
 //     }
 // }
 
-function listArrayItems(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        console.log(i, arr[i])
+// function listArrayItems(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         console.log(i, arr[i])
+//     }
+// }
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+// listArrayItems(colors);
+
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
     }
 }
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
-listArrayItems(colors);
+
+letterFinder("test", "s")
