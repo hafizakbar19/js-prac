394b8e4f7cbdced7af5902065f0079ddd2937547
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-14 08:57:27 +0500

    prac continue

--- app.js
+++ app.js
@@ -53,22 +53,34 @@
 
 // letterFinder("test", "s")
 
-function letterFinder(word, match) {
-    var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
-    var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
-    if(condition1 && condition2) { //if both condition matches
-        for(var i = 0; i < word.length; i++) {
-            if(word[i] == match) {
-                //check if the character at this i position in the word is equal to the match
-                console.log('Found the', match, 'at', i)
-            } else {
-                console.log('---No match found at', i)
-            }
-        }
-    } else {
-        //if the requirements don't match
-        console.log("Please pass correct arguments to the function")
-    }
-}
-letterFinder([],[])
-letterFinder("cat","c")
+// function letterFinder(word, match) {
+//     var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+//     var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
+//     if(condition1 && condition2) { //if both condition matches
+//         for(var i = 0; i < word.length; i++) {
+//             if(word[i] == match) {
+//                 //check if the character at this i position in the word is equal to the match
+//                 console.log('Found the', match, 'at', i)
+//             } else {
+//                 console.log('---No match found at', i)
+//             }
+//         }
+//     } else {
+//         //if the requirements don't match
+//         console.log("Please pass correct arguments to the function")
+//     }
+// }
+// letterFinder([],[])
+// letterFinder("cat","c")
+
+var bird = {
+    hasWings: true,
+    canFly: true,
+    hasFurr: true,
+    has4legs: false,
+};
+
+var sparrow = Object.create(bird);
+var parrot = Object.create(bird);
+
+console.log(parrot.has4legs);
