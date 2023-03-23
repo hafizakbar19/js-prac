911302f931a3e20b88b4b6d7014c491b4a074646
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-23 09:47:21 +0500

    object paradigm

--- app.js
+++ app.js
@@ -85,12 +85,35 @@
 
 // console.log(parrot.has4legs);
 
-var shoes = 100;
-var stateTax = 1.2;
 
-function totalPrice (shoes,tax){
-    return shoes * tax;
+//functional paradigm
+// var shoes = 100;
+// var stateTax = 1.2;
+
+// function totalPrice (shoes,tax){
+//     return shoes * tax;
+// }
+
+// var toPay = totalPrice(shoes,stateTax);
+// console.log(toPay);
+
+//object paradigm
+var purchase1 = {
+    shoes: 100,
+    stateTax: 1.2,
+    totalPrice: function(){
+        var calculation = purchase1.shoes * purchase1.stateTax;
+        console.log(`Total Price: `, calculation);
+    }
 }
+purchase1.totalPrice();
 
-var toPay = totalPrice(shoes,stateTax);
-console.log(toPay);
+var purchase2 = {
+    shoes: 50,
+    stateTax: 1.2,
+    totalPrice: function(){
+        var calculation = this.shoes * this.stateTax;
+        console.log(`Total Price: `, calculation);
+    }
+}
+purchase2.totalPrice();
