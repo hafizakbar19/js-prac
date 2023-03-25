87c247d92056eb18cff483ac749bc280b3a0301c
Mohammed Akbar Alli <hafizakbar19@gmail.com>
2023-03-25 23:59:48 +0500

    example

--- currency.js
+++ currency.js
@@ -1,11 +1,22 @@
-let currencyOne = 100;
-let currencyTwo = 0;
-let exchageRate = 278;
+// let currencyOne = 100;
+// let currencyTwo = 0;
+// let exchageRate = 278;
 
-function currencyConverter (amount,rate){
-    return amount * rate;
-}
+// function currencyConverter (amount,rate){
+//     return amount * rate;
+// }
 
-currencyTwo = currencyConverter(currencyOne,exchageRate);
+// currencyTwo = currencyConverter(currencyOne,exchageRate);
 
-console.log(currencyTwo);
+// console.log(currencyTwo);
+
+let counter = 3;
+function example (){
+    console.log(counter);
+    counter = counter -1;
+    if ( counter === 0) return;
+    example();
+
+};
+
+example();
